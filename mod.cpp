debe1b8751249ec82beab77684a5756107849f70
Björn Dahlgren <bjorn@dahlgren.io>
2022-02-11 02:50:24 +0100

    Initial release

---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "SFP: Sturkö - Livonia Edition";
+picture = "sfp_logo_ca.paa";
+dir = "@sfp_sturko_enoch";
+author = "Swedish Forces Pack";
+actionName = "Website";
+action = "http://www.anrop.se";
